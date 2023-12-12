3ad8489a8317fcb99d25b09eb44a3d65990864a6
SeyedMohammadHadi Hoseini <developerseyed@gmail.com>
2023-12-12 08:51:49 +0330

    second try

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name="fixer_demo",
-      version=0.3,
+      version=0.4,
       url="https://github.com/smhdeveloper/fixer_demo",
+      install_requires=['requests'],
       packages=["fixer"])
